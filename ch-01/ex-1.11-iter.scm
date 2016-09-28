f29653144aa62636c45ca194615ab441b8c0d6af
Maksym Pysanets <south_fish@map-2.local>
2016-09-28 22:19:30 +0200

    iterative implementation of f

--- ch-01/ex-1.11-iter.scm
+++ ch-01/ex-1.11-iter.scm
@@ -1,3 +1,6 @@
 (define (f n)
-  (define (iter args... count)
-  (iter args... n))
+  (define (iter a b c count)
+    (if (= count 0)
+	a
+	(iter b c (+ c (* 2 b) (* 3 a)) (- count 1))))
+  (iter 0 1 2 n))
