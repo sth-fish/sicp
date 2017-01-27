01effab5eeab009e637636694daa517cb5015ecc
Maksim Pysanets <m.pysanets@hotmail.com>
2017-01-27 13:11:57 +0100

    changed implementaion to use if statement, since the chapter is about if...

--- ch-01/ex-1.03.scm
+++ ch-01/ex-1.03.scm
@@ -1,8 +1,7 @@
-(define (max-two-sum-of-squares x y z)
-  (define max-1 (max x y))
-  (define min-1 (min x y))
-  (define max-2 (max z min-1))
-  (sum-of-squares max-1 max-2))
+(define (sum-of-max-two-squares x y z)
+  (if (> x y)
+      (sum-of-squares x (max y z))
+      (sum-of-squares y (max x z))))
 
 (define (sum-of-squares x y)
   (+ (square x) (square y)))
