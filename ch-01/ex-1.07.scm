bb4f35e45b242c2d82a24c0825e2a066345e77c6
Maksim Pysanets <m.pysanets@hotmail.com>
2017-02-02 11:44:23 +0100

    improved good-enough? procedure to work with small/large numbers

---
+++ ch-01/ex-1.07.scm
@@ -0,0 +1,29 @@
+; (sqrt (square 0.01))
+; => 0.03230844833048122
+; Test fails because absolute tolerance of 0.001 is too large
+; when computing sqrt of small numbers.
+
+;(sqrt 10000000000000)
+; For really large numbers evaluation never terminates due to limited precision
+
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (good-enough? guess x)
+  (< (abs (- (improve guess x) guess))
+     (abs (* guess 0.001))))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
